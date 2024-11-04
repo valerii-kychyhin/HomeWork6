2155cd18e5c864340afc2c634d1563df3ed548e7
nikol <nikol@DESKTOP-00DS0F5>
2024-11-04 15:46:35 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace HomeWork6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 466);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rock";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(547, 466);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 67);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Paper";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(891, 466);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 67);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Scissors";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(304, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(736, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Try to beat me in Rock, Paper, Scissors!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(304, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 46);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1319, 731);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Rock, Paper, Scisors game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            label1.Text = "Try to beat me in Rock, Paper, Scissors!";
+            label2.Text = ""; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PlayGame("Rock");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PlayGame("Paper");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PlayGame("Scissors");
+        }
+
+        private void PlayGame(string userChoice)
+        {
+            string computerChoice = GetComputerChoice();
+
+            string result = DetermineWinner(userChoice, computerChoice);
+
+           
+            label2.Text = $"You picked: {userChoice}\nComputer picked: {computerChoice}\nResult: {result}";
+        }
+
+        private string GetComputerChoice()
+        {
+            Random random = new Random();
+            int choice = random.Next(1, 4); 
+            if (choice == 1) return "Rock";
+            if (choice == 2) return "Paper";
+            return "Scissors";
+        }
+
+        private string DetermineWinner(string userChoice, string computerChoice)
+        {
+            if (userChoice == computerChoice)
+            {
+                return "Draw!";
+            }
+            else if ((userChoice == "Rock" && computerChoice == "Scissors") ||
+                     (userChoice == "Scissors" && computerChoice == "Paper") ||
+                     (userChoice == "Paper" && computerChoice == "Rock"))
+            {
+                return "You win!";
+            }
+            else
+            {
+                return "Computer wins!";
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            PlayGame("Paper");
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            PlayGame("Scissors");
+        }
+    }
+}
